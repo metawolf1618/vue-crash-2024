94459b62798283b6523a9f31c176587dbbc80cc5
metawolf1618 <qscorleone4@gmail.com>
2024-10-21 17:31:49 -0700

    commit all files

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+    export default {
+       setup () 
+        {
+        const name = 'John Doe';
+        const status = 'active';
+        const tasks = ['Task One', 'Task Two', 'Task Three'];
+        const toggleStatus = () => {
+
+          if (this.status ==='active') {
+          this.status = 'pending';
+        } else if (this.status === 'pending') {
+          this.status = 'inactive';
+        } else {
+          this.status = 'active';
+        }
+          }
+     }
+    };  
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status ==='pending'">User is pending</p>
+  <p v-esle>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link">Click for Google</a>
+  <br />
+  <button @click="toggleStatus">Change Status</button>
+</template>
+
